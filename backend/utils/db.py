e6b1b094624bcc43513df846d0b44c43a2da8e2f
lujip <jepijul@gmail.com>
2025-09-25 04:57:51 +0800

    added mongo lient address to db.py

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 # Connect to local MongoDB instance
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://admin:Ginabot7232@192.168.77.74:27017/mongodb_test?authSource=admin')
 
-db = client['mongodb_test']  # Use (or create) this database 
+db = client['recruitment']  # Use (or create) this database 
